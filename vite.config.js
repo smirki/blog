503214e2111825a28a2147737ce6c04c6978296c
Manav M. <kookygavinquack@gmail.com>
2025-02-20 09:09:00 -0500

    new host

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()] , 
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: "ai.golockedin.com", // Allow Vite to bind to this domain
+    strictPort: true, // Ensures Vite always uses the specified port
+    port: 4173, // You can change this if needed
+    cors: true // Allows cross-origin requests if needed
+  },
+  preview: {
+    host: "ai.golockedin.com",
+    port: 4173,
+  }
 })
